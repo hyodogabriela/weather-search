578f571cae824b1cc14793b04b9e57a4d9d5a56d
hyodogabriela <gahyodo@gmail.com>
2024-10-18 16:46:44 +0200

    Added a footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import './App.css'; // Importando o CSS
-import SearchEngine from './SearchEngine'; // Importando seu componente
+import './App.css'; 
+import SearchEngine from './SearchEngine'; 
 
 const App = () => {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <SearchEngine /> {/* Renderizando o componente SearchEngine */}
+      <SearchEngine />
+      <footer>
+        Open-sourced code by 
+        <a href="https://github.com/hyodogabriela/weather-search" target="_blank" rel="noopener noreferrer"> Gabriela Hyodo</a> 
+        and hosted on 
+        <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer"> Netlify</a>.
+      </footer>
     </div>
   );
 };
