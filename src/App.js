189eb582873dec6b24d6194b2d282cc7c2b894a5
hyodogabriela <gahyodo@gmail.com>
2024-10-18 16:31:40 +0200

    Updated App.js and index.js
    Changed the code so SearchEngine is rendered only at App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-function App() {
+import './App.css'; // Importando o CSS
+import SearchEngine from './SearchEngine'; // Importando seu componente
+
+const App = () => {
   return (
-    <div>
-      <h1>Weather Search!</h1>
+    <div className="App">
+      <h1>Weather App</h1>
+      <SearchEngine /> {/* Renderizando o componente SearchEngine */}
     </div>
   );
-}
+};
 
 export default App;
